933411d0d6d682e9e251c17462f1f77ee3b6b618
Adam Tyler <Adam.Tyler@ally.com>
2018-06-21 10:45:14 -0400

    Create controller tests

---
+++ src/test/java/com/el/ally/invoicify/api/BillingRecordControllerTests.java
@@ -0,0 +1,46 @@
+package com.el.ally.invoicify.api;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class BillingRecordControllerTests {
+
+	private BillingRecordController billingController;
+	
+	@Mock
+	private BillingRecordRepository billingRecordRepo;
+	
+	@Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        billingController = new BillingRecordController(billingRecordRepo);
+    }
+	
+	@Test
+	public void list_givenNoBillingRecords_shouldReturnEmptyList() {
+		ArrayList<BillingRecord> billingRecords = new ArrayList<BillingRecords>();
+		when(billingRecordRepo.findAll()).thenReturn(billingRecords);
+		
+		ArrayList<BillingRecord> actual = billingController.list();
+		
+		assertThat(actual.size()).isEqualTo(billingRecords.size());
+		verify(billingRecordRepo).findAll();
+	}
+	
+	@Test
+	public void list_givenBillingRecords_shouldReturnListOfAllRecords() {
+		ArrayList<BillingRecord> billingRecords = new ArrayList<BillingRecord>();
+		billingRecords.add(new FlatFeeBillingRecord());
+		billingRecords.add(new RateBasedBillingRecord());
+		when(billingRecordRepo.findAll()).thenReturn(billingRecords);
+		
+		ArrayList<BillingRecord> actual = billingController.list();
+		
+		assertThat(actual.size()).isEqualTo(billingRecords.size());
+		verify(billingRecordRepo).findAll();
+	}
+}
