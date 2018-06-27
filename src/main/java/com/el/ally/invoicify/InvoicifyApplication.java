57912850c7f8418f102c74be69ff505a8bdd584c
Adam Tyler <Adam.Tyler@ally.com>
2018-06-27 14:03:34 -0400

    Add CORS configuration

--- src/main/java/com/el/ally/invoicify/InvoicifyApplication.java
+++ src/main/java/com/el/ally/invoicify/InvoicifyApplication.java
@@ -2,6 +2,10 @@ package com.el.ally.invoicify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class InvoicifyApplication {
@@ -9,4 +13,5 @@ public class InvoicifyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InvoicifyApplication.class, args);
 	}
+	
 }
